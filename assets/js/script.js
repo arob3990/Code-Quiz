7f0b3213d33bedb2b80dc84fbb86eb2fab8ac216
Alex Robertson <alex.robertson390@gmail.com>
2022-11-20 18:54:03 -0800

    js sunday update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,35 @@
 var introSection = document.getElementById("intro");
 var quiz = document.getElementById("quiz");
 quiz.style.display = "none";
-
-
+var timerEl = document.getElementById("timer");
+var count = 60;
+var questionResult = document.getElementById("questionresult")
+var interval = null;
+var initialsButton= document.getElementById("enterInitials")
+var initialsInput = document.getElementById("initials")
+var showHighscores= document.getElementById("showHighscores")
+var highscores= document.getElementById("highscores");
+var listOfHighscores=document.getElementById("listOfHighscores");
 var startButton = document.getElementById("start");
 var score = 0;
 var results = document.getElementById("results");
-results.innerHTML=score
+var goHomeButton = document.getElementById("goHome")
+var endGame = false
+// results.innerHTML=score
+
 results.style.display="none"
 
+var allHighScores = JSON.parse(localStorage.getItem('highscores'))
+
 var startButton = document.getElementById("start");
+endGame = false;
 startButton.addEventListener("click", function(){
-    // startTimer();
+    startTimer();
     introSection.style.display = "none";
     renderNewQuestion(0);
 })
 
 
-
-// function nextQuestion(){
-//     index++;
-//     console.log(index);
-//     renderNewQuestion()
-// }
 
 var questions = [
     {
@@ -81,44 +88,183 @@ var questions = [
             label: "House",
             isCorrect: false,
         }]
+    },
+    {
+        title: "What is 4+4",
+        answers:[{
+            label: "7",
+            isCorrect: false,
+        },
+        {
+            label: "2",
+            isCorrect: false,
+        },
+        {
+            label: "8",
+            isCorrect: true,
+        },
+        {
+            label: "1",
+            isCorrect: false,
+        }]
+    },
+    {
+        title: "What is the first book in the Bible",
+        answers:[{
+            label: "Genesis",
+            isCorrect: true,
+        },
+        {
+            label: "Exodus",
+            isCorrect: false,
+        },
+        {
+            label: "Leviticus",
+            isCorrect: false,
+        },
+        {
+            label: "Matthew",
+            isCorrect: false,
+        }]
+    },
+    {
+        title: "How do you say 'Hello' in Spanish?",
+        answers:[{
+            label: "Adios",
+            isCorrect: false,
+        },
+        {
+            label: "Gracias",
+            isCorrect: false,
+        },
+        {
+            label: "Hola",
+            isCorrect: true,
+        },
+        {
+            label: "Hasta lluego",
+            isCorrect: false,
+        }]
+    },
+    {
+        title: "Which president got stuck in a bathtub?",
+        answers:[{
+            label: "Trump",
+            isCorrect: false,
+        },
+        {
+            label: "Obama",
+            isCorrect: false,
+        },
+        {
+            label: "Andrew Jackson",
+            isCorrect: false,
+        },
+        {
+            label: "William Taft",
+            isCorrect: true,
+        }]
+    },
+    {
+        title: "What is the meaning of the German word 'Panzer'?",
+        answers:[{
+            label: "Panther",
+            isCorrect: false,
+        },
+        {
+            label: "Gun",
+            isCorrect: false,
+        },
+        {
+            label: "Tank",
+            isCorrect: true,
+        },
+        {
+            label: "Wedding",
+            isCorrect: false,
+        }]
+    },
+    {
+        title: "What year is it?",
+        answers:[{
+            label: "2021",
+            isCorrect: false,
+        },
+        {
+            label: "2023",
+            isCorrect: false,
+        },
+        {
+            label: "2024",
+            isCorrect: false,
+        },
+        {
+            label: "2022",
+            isCorrect: true,
+        }]
+    },
+    {
+        title: "How many are in a bakers dozen?",
+        answers:[{
+            label: "11",
+            isCorrect: false,
+        },
+        {
+            label: "12",
+            isCorrect: false,
+        },
+        {
+            label: "13",
+            isCorrect: true,
+        },
+        {
+            label: "14",
+            isCorrect: false,
+        }]
     }
 
 ]
 
 
 
-var index =0;
-function renderNewQuestion(index){
-    if(index===3){
-        quiz.style.display="none"
-        results.innerHTML="Score " + score
-        results.style.display="block"
-    }
+// function displayResults(){
+//     // var resultsButton = document.createElement("button");
+//     // resultsButton.innerHTML = 'CLICK HERE FOR RESULTS';
+//     results.style.display="block"
 
+
+
+var index =0;
+function renderNewQuestion(){
+    if(index===10){
+    //     // quiz.style.display="none"
+    //     // results.innerHTML="Score " + score
+    //     // results.style.display="block"
+    //     // clearInterval(interval)
+    showResults()
+    }
     
-    console.log(index)
+    
+    console.log("This is the index",index)
     introSection.style.display="none";
     var questionName = document.createElement("h1");
     questionName.innerHTML = questions[index].title;
     var listOfAnswers = document.createElement("ol");
     for (var i=0;i<4;i++){
-        // var singleAnswer = document.createElement("li");
-        // var singleAnswer = quiz.appendChild(listofAnswers).createElement("button")
         var singleAnswerLi = document.createElement("li");
         var singleAnswer = document.createElement("button")
         singleAnswer.innerHTML = questions[index].answers[i].label
         if(questions[index].answers[i].isCorrect){
             singleAnswer.addEventListener("click",function(){
                 score+=10;
-                // getElementById(quiz).innerHTML = "";
-                // renderNewQuestion(index+1);
+                questionResult.innerHTML = "CORRECT";
                 clearQuestion();
             }) 
         } else {
             singleAnswer.addEventListener("click",function(){
-                // getElementById(quiz).innerHTML = "";
-                // renderNewQuestion(index+1);
+                questionResult.innerHTML = "INCORRECT";
                 clearQuestion();
+                count -= 10;
             }) 
         }
         singleAnswerLi.appendChild(singleAnswer)
@@ -131,50 +277,108 @@ function renderNewQuestion(index){
     console.log("question is number",index)
     
 }
+function showResults(){
+    endGame = true;
+    timerEl.style.display="none" //hide timer
+    //create a new p tag
+    results.removeChild(results.lastChild) // remove last p tag
+    var resultPTag = document.createElement("p");
+    //add text with the total points
+    resultPTag.innerHTML=score + " points";
+    results.appendChild(resultPTag);
+    results.style.display="block";
+    questionResult.style.display="none";
+}
+
 function clearQuestion(){
     document.getElementById("quiz").innerHTML="";
-    renderNewQuestion(index+1);
+    index++;
+    renderNewQuestion(index);
+    if(index > 11){
+        showResults()
+    }
+    //if index is bigger than the length of the array, then end it
 }
 
 
-// function renderNewQuestion(){
-//     quizSection.style.display = "block";
-//     var quizQuestion = document.getElementById("question");
+
+
+
+function startTimer(){
+    interval = setInterval(function(){
+    // timerEl.innerHTML="Time: ";
+    document.getElementById("timer").innerHTML="Time Remaining: "+ count;
+    count--;
+    if(count <= 0){
+        quiz.style.display='none';
+        results.style.display='block';
+        timerEl.style.display='none';
+        clearInterval(interval); // clear timer
+    }
+    if(endGame){
+        quiz.style.display='none';
+        results.style.display='block';
+        timerEl.style.display='none';
+        clearInterval(interval); // clear timer
+    }    
+
+    // if (count <= 0){
+    //   clearInterval(interval);
+    //   document.getElementById('timer').innerHTML='Done';
+    //   // or...
+    //   quiz.style.display ="none";
+    //   results.style.display="block"
+    // }
+  }, 1000);
+}
+
+initialsButton.addEventListener("click",function(){
+    var result={
+        initials:initialsInput.value,
+        points: score
+    }
+    // if highscore exists then append the new score, else, create the first highscore inside an array
+    if(allHighScores){
+        allHighScores.push(result)
+    } else{
+        allHighScores = [result]
+    }
+    localStorage.setItem('highscores', JSON.stringify(allHighScores))
+    initialsInput.value =""//reset input value
+    results.style.display="none";
+    intro.style.display="block";
+    questionResult.style.display = "none";
+    showHighscores.style.display="block"; //show button
     
-//     quizQuestion.textContent = questions[index].title
+})
 
-//     var choiceA = document.getElementById("choiceA")
-//     var choiceB = document.getElementById("choiceB")
-//     var choiceC = document.getElementById("choiceC")
-//     var choiceD = document.getElementById("choiceD")
+showHighscores.addEventListener('click',function(){
+    //hide everything
+    timerEl.style.display="none" //hide timer
+    showHighscores.style.display="none"; //hide button
+    intro.style.display="none";
+    quiz.style.display="none";
+    results.style.display="none";
+    questionResult.style.display ="none";
+    listOfHighscores.innerHTML="" //empty the scores
+    // allHighScores.sort(function(a,b){return b.points - a.points}) //sort highscores in descending order
+    for(var i=0;i<allHighScores.length;i++){
+        var newScoreElement= document.createElement('li');
+        newScoreElement.innerHTML=allHighScores[i].initials + " "+allHighScores[i].points
+        listOfHighscores.append(newScoreElement); //append each score to the list
+    }
+    highscores.style.display="block"; //show scores
 
-//     choiceA.textContent = questions[index].choices[0]
-//     choiceB.textContent = questions[index].choices[1]
-//     choiceC.textContent = questions[index].choices[2]
-//     choiceD.textContent = questions[index].choices[3]
+})
 
-//     choiceA.addEventListener("click", function(){
-//         if(choiceA === questions.correctAnswer){
-//             console.log(questions.correctAnswer)
-//             window.alert("YOU ANSWERED CORRECT")
-//         } else {
-//             window.alert("YOU ANSWERED INCORRECT")
-//         };
-//         nextQuestion()
-//     })
-//     choiceB.addEventListener("click", function(){
-//         if(choiceB === questions.correctAnswer){
-//             console.log(questions.correctAnswer)
-//             window.alert("YOU ANSWERED CORRECT")
-//         } else {
-//             window.alert("YOU ANSWERED INCORRECT")
-//         };
-//         nextQuestion()})
-//     choiceC.addEventListener("click", function(){
-//         nextQuestion()})
-//     choiceD.addEventListener("click", function(){
-//         nextQuestion()})
-    
-
-
-
+goHomeButton.addEventListener("click",function(){
+    quiz.style.display="none";
+    results.style.display="none";
+    highscores.style.display="none";
+    timerEl.innerHTML="Timer"
+    showHighscores.style.display="block";
+    intro.style.display="block";
+    timerEl.style.display="block";
+    questionResult.style.display= "none";
+    index=0;
+})
